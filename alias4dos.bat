96c382cb9bf62d49971b5e4b3c89951c791af1b7
chc1129 <chc1129@gmail.com>
2020-03-12 23:20:56 +0900

    windows alias file add

---
+++ alias4dos.bat
@@ -0,0 +1,17 @@
+chcp 65001
+doskey ls=dir /w
+doskey ll=dir /ad
+doskey cat=type $*
+doskey h=doskey /history $*
+doskey history=doskey /history $*
+doskey pwd=dir
+doskey mkdir=md
+doskey mv=move $*
+doskey rm=del
+doskey cp=copy $*
+doskey cat=type
+doskey touch=type nul $g $*
+doskey less=type
+doskey ifconfig=ipconfig
+doskey clear=cls
+doskey c=cls
